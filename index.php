256b9c42e2c6ae2e93f9400324fcaa048a7c03ac
gistartech <63531088+gistartech@users.noreply.github.com>
2020-04-11 23:03:49 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello</title>
+</head>
+<body>
+<p>Thank you for visiting us. This site is under construction</p>
+<?php
+date_default_timezone_set('America/Los_Angeles');
+echo date("Y-m-d h:i:s A");
+?>
+</body>
+</html>
